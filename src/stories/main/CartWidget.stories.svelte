1078243c0f08f70727b043ff4b9908be5f876a77
kavyabg <kavya@partskeys.com>
2024-09-18 18:30:03 +0530

    First Commit

---
+++ src/stories/main/CartWidget.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    import CartWidget from '$lib/components/ChemiKartWidget/CartWidget.svelte';
+  
+    export const meta = {
+      title: "CartWidget",
+      component: CartWidget
+    }
+  </script>
+  
+  <script>
+     import '../../app.css';
+     import { Story, Template } from '@storybook/addon-svelte-csf';
+  </script>
+  
+  <Template let:args>
+      <CartWidget> </CartWidget>
+    </Template>
+    
+    
+    <Story name="CartWidget1" />
