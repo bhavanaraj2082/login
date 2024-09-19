0b4bfcd701ef571039661232e809dfb3451c63d5
akshay sawant <akshay@partskeys.com>
2024-09-19 15:54:05 +0530

    initial commit for for contactus component

---
+++ src/stories/main/Contactus.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    import ContactUs from '$lib/components/ContactUs.svelte';
+  
+    export const meta = {
+      title: "ContactUs_form",
+      component: ContactUs
+    }
+  </script>
+  
+  <script>
+     import '../../app.css';
+     import { Story, Template } from '@storybook/addon-svelte-csf';
+  </script>
+  
+  <Template let:args>
+      <ContactUs /> 
+    </Template>
+    
+    
+    <Story name="Contact_us" />
