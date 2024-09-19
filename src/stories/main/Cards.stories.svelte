ba79e32b919d0f567c104826bcc31d37c52675da
santhosh <santhosh@partskeys.com>
2024-09-19 19:04:40 +0530

    Created Homepage components

---
+++ src/stories/main/Cards.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    import Cards from '../../lib/components/Homepagecomponents/Cards.svelte';
+  
+    export const meta = {
+      title: "Homepagecomponents",
+      component: Cards,
+    }
+  </script>
+  
+  <script>
+    import { Story, Template } from '@storybook/addon-svelte-csf';
+    import '../../app.css';
+  </script>
+  
+  <Template let:args>
+    <Cards/>
+  </Template>
+  
+  
+  <Story name="Cardshome" />
---
+++ src/stories/main/Upcomimgevent.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    import Upcomimgevent from '../../lib/components/Homepagecomponents/upcomimgevent.svelte';
+  
+    export const meta = {
+      title: "Homepagecomponents",
+      component: Upcomimgevent,
+    }
+  </script>
+  
+  <script>
+    import { Story, Template } from '@storybook/addon-svelte-csf';
+    import '../../app.css';
+  </script>
+  
+  <Template let:args>
+    <Upcomimgevent/>
+  </Template>
+  
+  
+  <Story name="Upcomimgeventhome" />
