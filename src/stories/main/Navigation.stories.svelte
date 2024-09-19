a232c1af9d1b57ba6040ad0fd6524bcc302f7648
bhavana <bhavanaraj@partskeys.com>
2024-09-19 12:06:23 +0530

    commit

---
+++ src/stories/main/Navigation.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    import Navigation from '$lib/components/Navigation.svelte';
+  
+    export const meta = {
+      title: "Navigation",
+      component: Navigation
+    }
+  </script>
+  
+  <script>
+     import '../../app.css';
+     import { Story, Template } from '@storybook/addon-svelte-csf';
+  </script>
+  
+  <Template let:args>
+      <Navigation> </Navigation>
+    </Template>
+    
+    
+    <Story name="Navigation1" />
