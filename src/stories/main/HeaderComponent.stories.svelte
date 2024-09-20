fe8066630a5fb4de2cdb85285acefbafb26b9e3e
sahana <sahana@partskeys.com>
2024-09-20 16:07:39 +0530

    initial commit

---
+++ src/stories/main/HeaderComponent.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  import HeaderComponent from '../../lib/components/HeaderComponent/HeaderComponent.svelte';
+
+  export const meta = {
+    title: "Header Section",
+    component: HeaderComponent,
+  }
+</script>
+
+<script>
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+  import '../../app.css';
+</script>
+
+<Template let:args>
+  <HeaderComponent/>
+</Template>
+
+
+<Story name="HeaderComponentt" />
