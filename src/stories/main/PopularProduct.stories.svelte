ca280e71effaa0308e0be8d83790f417bc4cd13a
k pavan kumar <pavan@partskeys.com>
2024-10-14 18:43:51 +0530

    created popular products

---
+++ src/stories/main/PopularProduct.stories.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+    import PopularProduct from '../../lib/components/PopularProduct.svelte';
+    
+    export const meta = {
+      title: "PopularProduct",
+      component: PopularProduct,
+    }
+  </script>
+  
+  <script>
+    import { Story, Template } from '@storybook/addon-svelte-csf';
+    import '../../app.css';
+  </script>
+  
+  <Template let:args>
+    <PopularProduct />
+  </Template>
+  
+  
+  <Story name="PopularProduct1" />
+  
