dc7f5d87841da98ab1c6d85caf6c37ceebfc2b08
hemanth <hemanth@partskeys.com>
2024-10-15 11:29:13 +0530

    added img from static

--- src/lib/components/Herosection.svelte
+++ src/lib/components/Herosection.svelte
@@ -1,12 +1,5 @@
-<script>
-    import hero from "../../lib/data/Herosection/hero.png"
-</script>
-
-
 <div class="text-sm ">
     <section class="flex items-center justify-center my-5 mx-1 flex-col-reverse sm:flex-row">
-
-
         <div class="sm:w-5/12 text-center sm:text-left ">
             <h1 class="m-2 md:mt-14 sm:mt-10 font-bold md:text-2xl text-lg">
                 Empowering Science with Quality Chemicals.
@@ -16,22 +9,20 @@
                 Welcome to Chemikart, your premier destination for high-quality chemicals and laboratory supplies.
             </p>
             <a href="#" 
-                class="w-28 text-center inline-block rounded border-1 border-orange-200 text-white bg-orange-400  md:text-xs py-2  font-semibold hover:bg-orange-300  transition">
+                class="w-28 text-center inline-block rounded border-1 border-primary-200 text-primary-50 bg-primary-500  md:text-xs py-2  font-semibold hover:bg-primary-400  transition">
                 Shop Now
             </a>
             <a href="#" 
-                class="w-28 text-center inline-block my-2 mx-5 rounded border border-orange-400 text-orange-400 bg-white md:text-xs py-2   font-semibold hover:bg-orange-400 hover:text-white transition" >
+                class="w-28 text-center inline-block my-2 mx-5 rounded border border-primary-400 text-primary-400 bg-primary-50 md:text-xs py-2   font-semibold hover:bg-primary-500 hover:text-primary-50 transition" >
                 View All
             </a>
         </div>
-
-
                 <!-- Images Section -->
                 <div class="sm:w-4/12 w-5/12">
                     <img 
-                        src={hero} 
+                        src="/hero.png"
                         class=" "  
-                        alt="" />
+                        alt="img" />
                 </div>
     </section>
 </div>
