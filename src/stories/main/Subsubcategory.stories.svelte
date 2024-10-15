0ee6e7d3d27a119650063833d93973eb09e6cad4
bhavana <bhavanaraj@partskeys.com>
2024-10-15 11:52:13 +0530

    sub sub category information page

---
+++ src/stories/main/Subsubcategory.stories.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+    import Subsubcategory from "$lib/components/Subsubcategory.svelte";
+
+    export const meta = {
+        title: "Subsubcategory",
+        component: Subsubcategory,
+    };
+</script>
+
+<script>
+    import "../../app.css";
+    import { Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template>
+    <Subsubcategory />
+</Template>
+
+<Story name="Subsubcategory12"/>
