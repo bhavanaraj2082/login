43bab8783b827c32dd92ffa92524504aed3ba8f8
bhavanaraj <bhavanaraj@partskeys.com>
2024-10-15 14:20:33 +0000

    Update src/lib/components/Subsubcategory.svelte

--- src/lib/components/Subsubcategory.svelte
+++ src/lib/components/Subsubcategory.svelte
@@ -5,36 +5,35 @@
     let category = [];
 
     onMount(() => {
-        
-        category = Array.isArray(productsData.products) ? productsData.products : [];
-       // console.log(category); 
+        category = productsData.products || [];
     });
+   
 </script>
 
 <div class="container mx-auto px-4">
-    <div class="flex flex-col">
+    <div class="flex flex-col ">
         {#each category as product, index}
-            <div class="flex flex-col md:flex-row bg-white p-6 shadow-sm transition-transform duration-300">
+            <div class="bg-white p-6 shadow-sm transition-transform duration-300 w-full flex flex-col md:flex-row">
                 {#if index % 2 === 0}
-                    <img 
-                        src={product.image_path} 
-                        alt={product.type} 
-                        class="w-full h-auto object-contain mb-4 md:mb-0 md:w-24 lg:w-1/4 mx-auto" 
-                    />
-                    <div class="flex-1 text-center md:text-left mt-4 md:mt-0 md:ml-4">
+                    <div class="w-full md:w-1/3 mb-4 md:mb-0">
+                        <img src={product.image_path} alt={product.type} class="w-full h-auto object-contain" />
+                    </div>
+                    <div class="flex-1 text-center md:text-left md:ml-8"> 
                         <h2 class="font-semibold text-lg mb-2 text-orange-500">{product.type}</h2>
                         <p class="text-base mb-2">{product.description}</p>
                     </div>
                 {:else}
-                    <div class="flex-1 text-center md:text-left mt-4 md:mt-0 md:mr-4">
+                    <div class="flex-1 text-center md:text-left md:mr-8 hidden md:block"> 
                         <h2 class="font-semibold text-lg mb-2 text-orange-500">{product.type}</h2>
                         <p class="text-base mb-2">{product.description}</p>
                     </div>
-                    <img 
-                        src={product.image_path} 
-                        alt={product.type} 
-                        class="w-full h-auto object-contain mb-4 md:mb-0 md:w-24 lg:w-1/4 mx-auto" 
-                    />
+                    <div class="w-full md:w-1/3 mb-4 md:mb-0">
+                        <img src={product.image_path} alt={product.type} class="w-full h-auto object-contain" />
+                    </div>
+                    <div class="flex-1 text-center md:hidden"> 
+                        <h2 class="font-semibold text-lg mb-2 text-orange-500">{product.type}</h2>
+                        <p class="text-base mb-2">{product.description}</p>
+                    </div>
                 {/if}
             </div>
         {/each}
