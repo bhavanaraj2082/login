0e247ec89a17aa412de6e4258c2d9584cf212a7e
sushma <sushma@partskeys.com>
2024-10-16 17:18:53 +0530

    commit

---
+++ src/stories/main/Shoppingcart.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    import ShoppingCart from '$lib/components/Cart/ShoppingCart.svelte';
+  
+    export const meta = {
+      title: "ShoppingCart",
+      component: ShoppingCart
+    }
+  </script>
+  
+  <script>
+     import '../../app.css';
+     import { Story, Template } from '@storybook/addon-svelte-csf';
+  </script>
+  
+  <Template let:args>
+      <ShoppingCart> </ShoppingCart>
+    </Template>
+    
+    
+    <Story name="ShoppingCart1" />
