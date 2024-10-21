25a3532d2c31e9d0601f7ab2b2d16c92f03458f0
k pavan kumar <pavan@partskeys.com>
2024-10-21 15:26:52 +0530

    Hero section updated as per design

--- src/lib/components/Herosection.svelte
+++ src/lib/components/Herosection.svelte
@@ -1,27 +1,29 @@
-<div class="text-sm ">
-    <section class="flex items-center justify-center my-5 mx-1 flex-col-reverse sm:flex-row">
-        <div class="sm:w-5/12 text-center sm:text-left ">
-            <h1 class="m-2 md:mt-14 sm:mt-10 font-bold md:text-2xl text-lg">
+<div class="w-11/12 mx-auto py-12 ">
+    <section class="flex items-center justify-center flex-wrap-reverse md:flex-nowrap gap-4">
+        <div class="text-center md:text-left mx-4 w-full md:w-6/12">
+            <h1 class="my-2 sm:m-0 md:mt-14 sm:mt-10 font-bold md:text-3xl text-2xl">
                 Empowering Science with Quality Chemicals.
             </h1>
             
-            <p class="py-6 m-2 text-gray-400 text-xs md:text-base font-medium">
+            <p class="my-2 text-gray-400 text-xs md:text-base font-medium">
                 Welcome to Chemikart, your premier destination for high-quality chemicals and laboratory supplies.
             </p>
+            <div class="flex md:justify-start justify-center gap-8 mt-3">
             <a href="#" 
-                class="w-28 text-center inline-block rounded border-1 border-primary-200 text-primary-50 bg-primary-500  md:text-xs py-2  font-semibold hover:bg-primary-400  transition">
+                class="px-6  rounded  bg-primary-400 text-primary-50  text-xs  py-2  font-semibold hover:bg-primary-400  transition">
                 Shop Now
             </a>
             <a href="#" 
-                class="w-28 text-center inline-block my-2 mx-5 rounded border border-primary-400 text-primary-400 bg-primary-50 md:text-xs py-2   font-semibold hover:bg-primary-500 hover:text-primary-50 transition" >
+                class="px-6  rounded border border-primary-400 text-primary-400 bg-primary-50 text-xs  py-2  font-semibold hover:bg-primary-400 hover:text-primary-50 transition" >
                 View All
             </a>
         </div>
+        </div>
                 <!-- Images Section -->
-                <div class="sm:w-4/12 w-5/12">
+                <div class="mx-4 w-7/12 md:w-5/12">
                     <img 
                         src="/hero.png"
-                        class=" "  
+                        class="w-full "  
                         alt="img" />
                 </div>
     </section>
