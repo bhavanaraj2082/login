f68f4754340732a7f022afb016947c85523de529
bhavana <bhavanaraj@partskeys.com>
2024-11-08 19:41:16 +0530

    Footer Links

--- src/lib/components/Footerlinks/Condition.svelte
+++ src/lib/components/Footerlinks/Condition.svelte
@@ -2,7 +2,7 @@
     import Condition from '$lib/data/FooterLinks/condition.json'
     </script>
     <div>
-        <h1 class="text-3xl font-bold mb-4 mt-10 text-center">General Terms and Conditions of Sale
+        <h1 class="text-2xl font-bold mb-4 mt-10 text-center">General Terms and Conditions of Sale
         </h1>
         <p class="mt-2 font-bold">Rev. April 2021</p>
         <ol class="list-decimal pl-5 mt-5 space-y-2 text-primary-400">
@@ -14,48 +14,53 @@
             <li><a href="#Limited Warranties" class="hover:underline">Limited Warranties</a></li>
             <li><a href="#Returns" class="hover:underline">Returns </a></li>
             <li><a href="#Termination" class="hover:underline">Termination</a></li>
-            <li><a href="#covid" class="hover:underline">COVID-19 Disclaimer</a></li>
+         
     
         </ol>
         <hr class="border-t-1 mt-4 border border-gray-400 mb-10">
         <h3 id="Applicability" class="text-primary-400 mt-5 text-lg">1. Applicability</h3>
-        <p class="mt-2">1.1 {Condition.Condition.Applicabilty}</p>
-        <p class="mt-2">1.2 {Condition.Condition.Applicabilty1}</p>
-        <p class="mt-2">1.3 {Condition.Condition.Applicabilty2}</p>
-        <p class="mt-2">1.4 {Condition.Condition.Applicabilty3}</p>
+        <p class="mt-2 text-sm text-justify">1.1 {Condition.Condition.Applicabilty}</p>
+        <p class="mt-3  text-sm text-justify">1.2 {Condition.Condition.Applicabilty1}</p>
+        <p class="mt-3 text-sm text-justify">1.3 {Condition.Condition.Applicabilty2}</p>
+        <p class="mt-3  text-sm text-justify">1.3 {Condition.Condition.Applicabilty2}</p>
+        <p class="mt-3 text-sm   text-justify">1.3 {Condition.Condition.Applicabilty2}</p>
+        <p class="mt-3 text-sm text-justify">1.4 {Condition.Condition.Applicabilty3}</p>
         <h3 id="Delivery and Performance" class="text-primary-400 mt-5 text-lg">2.Delivery and Performance</h3>
-        <p class="mt-2">2.1 {Condition.Condition.Delivery}</p>
-        <p class="mt-2">2.2 {Condition.Condition.Delivery1}</p>
-        <p class="mt-2">2.3 {Condition.Condition.Delivery2}</p>
-        <p class="mt-2">2.4 {Condition.Condition.Delivery3}</p>
+        <p class="mt-3 text-sm text-justify">2.1 {Condition.Condition.Delivery}</p>
+        <p class="mt-3 text-sm  text-justify">2.2 {Condition.Condition.Delivery1}</p>
+        <p class="mt-3 text-sm text-justify">2.3 {Condition.Condition.Delivery2}</p>
+        <p class="mt-3 text-sm text-justify">2.4 {Condition.Condition.Delivery3}</p>
         <h3 id="Use of Products" class="text-primary-400 mt-5 text-lg">3.Use of Products</h3>
-        <p class="mt-2">3.1 {Condition.Condition.use_product}</p>
-        <p class="mt-2">3.2 {Condition.Condition.use_product1}</p>
-        <p class="mt-2">3.3 {Condition.Condition.use_product2}</p>
+        <p class="mt-3 text-sm text-justify">3.1 {Condition.Condition.use_product}</p>
+        <p class="mt-3 text-sm text-justify">3.2 {Condition.Condition.use_product1}</p>
+        <p class="mt-3 text-sm text-justify">3.3 {Condition.Condition.use_product2}</p>
         <h3 id="Price and Payment" class="text-primary-400 mt-5 text-lg">4.Price and Payment</h3>
-        <p class="mt-2">4.1 {Condition.Condition.payment}</p>
-        <p class="mt-2">4.2 {Condition.Condition.payment1}</p>
-        <p class="mt-2">4.3 {Condition.Condition.payment2}</p>
-        <p class="mt-2">4.4 {Condition.Condition.payment3}</p>
-        <p class="mt-2">4.5 {Condition.Condition.payment4}</p>
+        <p class="mt-3  text-sm text-justify">4.1 {Condition.Condition.payment}</p>
+        <p class="mt-3 text-sm text-justify">4.2 {Condition.Condition.payment1}</p>
+        <p class="mt-3 text-sm text-justify">4.3 {Condition.Condition.payment2}</p>
+        <p class="mt-3 text-sm text-justify">4.4 {Condition.Condition.payment3}</p>
+        <p class="mt-3 text-sm text-justify">4.5 {Condition.Condition.payment4}</p>
         <h3 id="Software and Use Documents Licence Terms" class="text-primary-400 mt-5 text-lg">5.Software and Use Documents Terms</h3>
-        <p class="mt-2">5.1 {Condition.Condition.document}</p>
-        <p class="mt-2">5.2 {Condition.Condition.document1}</p>
-        <p class="mt-2">5.3 {Condition.Condition.document2}</p>
-        <p class="mt-2">5.4 {Condition.Condition.document3}</p>
+        <p class="mt-3 text-sm text-justify">5.1 {Condition.Condition.document}</p>
+        <p class="mt-3 text-sm text-justify">5.2 {Condition.Condition.document1}</p>
+        <p class="mt-3 text-sm text-justify">5.3 {Condition.Condition.document2}</p>
+        <p class="mt-3 text-sm text-justify">5.4 {Condition.Condition.document3}</p>
         <h3 id="Returns" class="text-primary-400 mt-5 text-lg">6.Returns</h3>
-        <p class="mt-2">{Condition.Condition.returns}</p>
+        <p class="mt-3 text-sm text-justify">{Condition.Condition.returns}</p>
         <h3 id="Limited Warranties" class="text-primary-400 mt-5 text-lg">7.Limited Warranties</h3>
-        <p class="mt-2">7.1 {Condition.Condition.waranty}</p>
+        <p class="mt-3 text-sm text-justify">7.1 {Condition.Condition.waranty}</p>
        
-        <p class="mt-2">7.2 {Condition.Condition.waranty2}</p>
-        <p class="mt-2">7.3 {Condition.Condition.waranty3}</p>
-        <p class="mt-2">7.4 {Condition.Condition.waranty4}</p>
-        <p class="mt-2">7.5 {Condition.Condition.waranty5}</p>
-        <p class="mt-2">7.6 {Condition.Condition.waranty6}</p>
-        <p class="mt-2">7.7 {Condition.Condition.waranty7}</p>
+        <p class="mt-3 text-sm text-justify">7.2 {Condition.Condition.waranty2}</p>
+        <p class="mt-3 text-sm text-justify">7.3 {Condition.Condition.waranty3}</p>
+        <p class="mt-3 text-sm text-justify">7.4 {Condition.Condition.waranty4}</p>
+        <p class="mt-3 text-sm text-justify">7.5 {Condition.Condition.waranty5}</p>
+        <p class="mt-3 text-sm text-justify">7.6 {Condition.Condition.waranty6}</p>
+        <p class="mt-3 text-sm text-justify">7.7 {Condition.Condition.waranty7}</p>
         <h3 id="Termination" class="text-primary-400 mt-5 text-lg">8.Termination</h3>
-        <p class="mt-2">{Condition.Condition.termination}</p>
-        <h3 id="Termination" class="text-primary-400 mt-5 text-lg">9. COVID-19 Disclaimer</h3>
-        <p class="mt-2">{Condition.Condition.covid}</p>
-       </div>
+        <p class="mt-3 text-sm text-justify">{Condition.Condition.termination}</p>
+       
+    
+    
+        
+    
+    </div>
