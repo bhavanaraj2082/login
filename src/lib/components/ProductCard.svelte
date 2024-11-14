e80e2e2ae99e272c5e81c22325f88f6b4a30c25d
akshay sawant <akshay@partskeys.com>
2024-11-14 10:56:14 +0530

    added

---
+++ src/lib/components/ProductCard.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let product;
+</script>
+
+<div
+    class="shadow-sm flex flex-col justify-between border bg-white rounded-lg pb-6 overflow-hidden"
+>
+    <img
+        src={product?.imageSrc}
+        alt={product?.productName}
+        class="w-48 h-48 mx-auto object-contain rounded-t-lg mb-4"
+    />
+    <div class="flex-1 space-y-2">
+        <h3 class="text-xl font-semibold">{product?.productName}</h3>
+        <p class="text-gray-600 h-16 p-2">{product?.prodDesc}</p>
+    </div>
+    <a
+        href={product?.productNumber || "#"}
+        class="bg-primary-400 text-white w-5/6 mx-auto py-2.5 rounded hover:bg-primary-500 mt-4"
+    >
+        View Details
+    </a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,12 @@
   import PopularProduct from '$lib/components/PopularProduct.svelte';
   import AboutSection from '$lib/components/AboutSection.svelte';
   import Email from '$lib/components/Email.svelte';
+  export let data;
+  let PopularProducts=data.PopularProductsData
 </script>
 <Herosection/>
 <FeaturedLinks/>
-<PopularProduct/>
+<PopularProduct {PopularProducts}/>
 <AboutSection/>
 <Email/>
 
