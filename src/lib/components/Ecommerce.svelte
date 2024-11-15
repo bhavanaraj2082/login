fd84df187f15626c4bf0cef63a1e03bdf02dbea4
sushma <sushma@partskeys.com>
2024-11-15 18:33:45 +0530

    commit

--- src/lib/components/Ecommerce.svelte
+++ src/lib/components/Ecommerce.svelte
@@ -1,16 +1,10 @@
 <script>
     import Icon from '@iconify/svelte';
-    let selectedOptions = {
-        'existing-connection': false,
-        'more-info': false,
-        'setup-connection': false,
-        'existing-organization': false,
-        'other-question': false,
-    };
-    function handleOptionChange(option) {
-        selectedOptions[option] = !selectedOptions[option];
-    }
-        import { enhance } from '$app/forms';
+    import { tick } from 'svelte';
+    let selectedNames = [];
+    let reason = [];
+    $: reason = [...selectedNames];
+    import { enhance } from '$app/forms';
     const locations = ["India", "USA", "UK", "Australia", "Canada", "Germany", "France", "Japan", "Brazil", "South Africa"];
     let phone = ''; 
     let name = '';
@@ -44,6 +38,7 @@
     details='';
     company='';
     role='';
+    selectedNames=[];
 }
     function handleSubmit(event) {
         if (number.length !== 10 || email.length === 0 || fname.length === 0 || company.length === 0 || details.length === 0 || role.length===0 || lname.length === 0) {
@@ -59,17 +54,25 @@
         if (!formValid) { 
             event.preventDefault(); 
         }
-    }
+    }    
     function showMessage(message1, keywordError) {
-        successMessage = message1;
-        errorMessage = keywordError;
-        if (keywordError === 'success') {
-        setTimeout(() => {resetForm();}, 2000);  
+    successMessage = message1;
+    errorMessage = keywordError;
+    const messageContainer = document.querySelector('.message-container');
+    if (messageContainer) {
+        setTimeout(() => {
+            messageContainer.scrollIntoView({ behavior: 'smooth' });
+        }, 100); 
     }
+    if (keywordError === 'success') {
+        setTimeout(() => {
+            resetForm();
+        }, 2000);
     }
+}
 </script>
 <section class="mt-16 md:px-16 px-5 md:mt-10">
-    <div class="hover:text-primary-300 text-primary-500 text-xl font-semibold mb-7">Chemikart Solutions</div>
+    <div class="hover:text-primary-300 text-primary-500 text-3xl font-semibold mb-7">Chemikart Solutions</div>
         <p class="text-base leading-6 text-content mx-auto text-justify-center font-montserrat">
         We offer a full range of e-Commerce tools designed to enable greater spend control for your procurement staff, providing your researchers with ease of access to the products they've come to rely on.
         </p>
@@ -83,21 +86,21 @@
                         <div class="flex flex-col md:flex-row items-start gap-4">
                             <Icon icon="game-icons:on-target" class="w-10 h-10 text-primary-500 flex-shrink-0" />
                             <div class="flex-auto">
-                                <h2 class="text-heading font-semibold text-lg font-worksans text-primary-500">The One-to-One Solution: E-Procurement</h2>
+                                <h2 class="font-semibold text-lg font-worksans text-primary-500">The One-to-One Solution: E-Procurement</h2>
                                 <p class="text-content font-montserrat mt-4">e-Procurement provides seamless system-to-system connectivity of your existing spend management system.</p>
                             </div>
                         </div>
                         <div class="flex flex-col md:flex-row items-start gap-4">
                             <Icon icon="mdi:family-tree" class="w-10 h-8 text-primary-500 flex-shrink-0" />
                             <div class="flex-auto">
-                                <h2 class="text-heading font-semibold text-lg font-worksans text-primary-500">The Customized Solution: Pipeline®</h2>
+                                <h2 class="font-semibold text-lg font-worksans text-primary-500">The Customized Solution: Pipeline®</h2>
                                 <p class="text-content font-montserrat mt-4">This customized service balances your organization's business needs with the product requirements of your researchers.</p>
                             </div>
                         </div>
                         <div class="flex flex-col md:flex-row items-start gap-4">
                             <Icon icon="fluent-mdl2:text-document-shared" class="w-10 h-8 text-primary-500 flex-shrink-0" />
                             <div class="flex-auto">
-                                <h2 class="text-heading font-semibold text-lg font-worksans text-primary-500">The Simple Solution: Chemikart E-Shop</h2>
+                                <h2 class="font-semibold text-lg font-worksans text-primary-500">The Simple Solution: Chemikart E-Shop</h2>
                                 <p class="text-content font-montserrat mt-4">Your procurement staff can access our products simply, easily, and securely through chemikart.com.</p>
                             </div>
                         </div>
@@ -115,7 +118,6 @@
                     </div>
                 </aside>
             </div>
-            
         </div>
 </section>
 <section class="mx-4 sm:mx-8 md:mx-10">
@@ -125,14 +127,14 @@
     <div class="flex gap-8 max-md:flex-col">
         <div class="flex flex-col w-[65%] max-md:w-full">
             <div class="flex flex-col text-sm leading-6 text-black max-md:mt-6">
-                <h3 class="text-heading font-semibold text-lg font-worksans mt-8 text-primary-500">The One-to-One Solution: E-Procurement</h3>
+                <h3 class="font-semibold text-lg font-worksans mt-8 text-primary-500">The One-to-One Solution: E-Procurement</h3>
                 <p class="mt-4 text-base text-content font-montserrat">
                     Chemikart is your source for procurement optimization. If you are already using an electronic spend management solution, leverage your investment to generate even greater savings by automating Chemikart's purchases and link your current buying platform to us.
                 </p>
                 <p class="mt-4 text-base text-content font-montserrat">
                     We support all major e-Procurement providers and have a dedicated technical team available to work with your organization or platform provider and install customized, scalable, all-in-one procurement solutions. e-Procurement offers efficiencies across the purchasing cycle, enabling product selection through e-Catalogs, both punchout or hosted file, accepting e-Purchase orders, sending electronic invoices, order confirmations, and advanced shipping notices, and more.
                 </p>
-                <h4 class="mt-6 text-heading font-semibold text-lg font-worksans text-primary-500">Three Steps to Install e-Procurement</h4>
+                <h4 class="mt-6 font-semibold text-lg font-worksans text-primary-500">Three Steps to Install e-Procurement</h4>
                 <ol class="list-decimal list-inside mt-4 space-y-3 text-base text-content font-montserrat">
                     <li>Create a Project Plan: We will partner with you to gather requirements, provide detailed analysis, then implement a project plan with milestones.</li>
                     <li>Setup & Testing: We will work through setup and testing to ensure end-to-end processes are in place.</li>
@@ -141,7 +143,7 @@
             </div>
         </div>
         <div class="flex flex-col w-[35%] max-md:w-full max-md:mt-6">
-            <h3 class="text-heading font-semibold text-lg font-worksans mt-8 text-primary-500">Benefits to e-Procurement</h3>
+            <h3 class="font-semibold text-lg font-worksans mt-8 text-primary-500">Benefits to e-Procurement</h3>
             <ol class="list-decimal list-inside mt-4 space-y-3 text-base text-content font-montserrat">
                 <li>Process efficiency from end-to-end driven by real-time data exchange, speed, and transaction efficiency.</li>
                 <li>Cost Reduction via reduced transaction costs and administrative overhead.</li>
@@ -156,7 +158,7 @@
     </div>
     <div class="flex gap-8 max-md:flex-col mt-12">
         <div class="flex flex-col w-[65%] max-md:w-full">
-            <h3 class="text-heading font-semibold text-lg font-worksans mt-8 text-primary-500">The Customized Solution: Pipeline®</h3>
+            <h3 class="font-semibold text-lg font-worksans mt-8 text-primary-500">The Customized Solution: Pipeline®</h3>
             <p class="mt-4 text-base text-content font-montserrat">
                 An advanced ordering process customized to meet your business needs. PIPELINE® delivers options and benefits beyond simple ordering, providing direct, private connection to our order processing and fulfillment system. Flexible ordering processes are customized to meet your organization's requirements.
             </p>
@@ -168,7 +170,7 @@
             </p>
         </div>
         <div class="flex flex-col w-[35%] max-md:w-full max-md:mt-6">
-            <h4 class="text-heading font-semibold text-lg font-worksans mt-8 text-primary-500">Benefits of PIPELINE®</h4>
+            <h4 class="font-semibold text-lg font-worksans mt-8 text-primary-500">Benefits of PIPELINE®</h4>
             <ul class="list-disc list-inside mt-4 space-y-2 text-base text-content font-montserrat">
                 <li>Elimination of paper transactions</li>
                 <li>No IT investment required</li>
@@ -184,7 +186,7 @@
     </div>
     <div class="flex gap-8 max-md:flex-col mt-12">
         <div class="flex flex-col w-[65%] max-md:w-full">
-            <h3 class="text-heading font-semibold text-lg font-worksans mt-8 text-primary-500">The Simple Solution: Chemikart E-Shop</h3>
+            <h3 class="font-semibold text-lg font-worksans mt-8 text-primary-500">The Simple Solution: Chemikart E-Shop</h3>
             <p class="mt-4 text-base text-content font-montserrat">
                 Your online order is integrated with our fulfillment systems, allowing for rapid processing. You can request quotes, place orders, and track deliveries all in one place.
             </p>
@@ -193,7 +195,7 @@
             </p>
         </div>
         <div class="flex flex-col w-[35%] max-md:w-full max-md:mt-6">
-            <h4 class="text-heading font-semibold text-lg font-worksans mt-8 text-primary-500">Benefits of the Chemikart e-Shop:</h4>
+            <h4 class="font-semibold text-lg font-worksans mt-8 text-primary-500">Benefits of the Chemikart e-Shop:</h4>
             <ul class="list-disc list-inside mt-4 space-y-2 text-base text-content font-montserrat">
                 <li>Your information is safe and secure</li>
                 <li>Advanced tools and product searches</li>
@@ -210,29 +212,7 @@
 <hr class="self-center mt-20 h-px w-full border border-black border-opacity-40 max-md:mt-10" />
 </section>
 <section class="flex flex-col items-start px-4 sm:px-8 md:px-12 py-6 mt-16 max-w-full w-full max-md:px-2 max-md:mt-10">
-<h2 class="text-heading font-semibold text-lg font-worksans text-primary-500 ml-3">Contact Our Team</h2>
-<fieldset class="mb-8">
-        <legend class="mt-8 ml-2.5 text-lg leading-relaxed font-semibold text-heading font-worksans max-md:ml-1.5 text-4s text-primary-500">Select all that apply</legend>
-        <div class="flex flex-col gap-4 mt-4 ml-2.5 text-lg leading-relaxed text-content font-montserrat max-md:ml-1.5 text-4s">
-            {#each Object.keys(selectedOptions) as option}
-                <div class="flex items-center gap-3">
-                    <input type="checkbox" id={option} class="sr-only" 
-                            checked={selectedOptions[option]} 
-                            on:change={() => handleOptionChange(option)} />
-                    <label for={option} class="flex items-center cursor-pointer">
-                        <Icon icon={selectedOptions[option] ? "ci:checkbox-checked" : "ci:checkbox-unchecked"} alt="" class="w-5 h-5 object-contain shrink-0 text-primary-500" />
-                        <span class="ml-2">
-                            {option === 'existing-connection' ? "I have a question about an existing B2B connection." :
-                            option === 'more-info' ? "I would like more information about B2B solutions" :
-                            option === 'setup-connection' ? "I would like to be contacted about setting up a B2B connection" :
-                            option === 'existing-organization' ? "I would like to know if there is an existing B2B connection for my organization" :
-                            "I have another question (fill in below)"}
-                        </span>
-                    </label>
-                </div>
-            {/each}
-        </div>
-    </fieldset>
+<h2 class="font-semibold text-3xl font-worksans text-primary-500 ml-3">Contact Our Team</h2>
 <form method="POST" action="?/contactus" class="w-full mt-3 max-w-3xl ml-3" use:enhance={() => {
     return async({ result }) => {
     let message1 = '';
@@ -251,7 +231,7 @@
     showMessage(message1, keywordError);
     };
 }}>
-    <div class="mt-3">
+    <div class="message-container mt-3">
         {#if errorMessage === "success"}
             <div class="text-center bg-green-100 text-green-700 py-2 mb-4 rounded-md">
                 {successMessage}
@@ -261,6 +241,31 @@
                     {successMessage}
                 </div>
             {/if}
+            </div>
+            <div>
+                <p class="mb-5 mt-10 text-lg font-semibold text-primary-500">Select all that apply</p>
+            </div>
+            <div>
+                <label class="flex items-center space-x-2 text-lg">
+                    <input type="checkbox"  bind:group={selectedNames} class="w-6 h-6 mb-5 form-checkbox accent-primary-500" value="I have a question about an existing B2B connection "/>
+                    <span class="mb-5">I have a question about an existing B2B connection.</span>
+                </label>
+                <label class="flex items-center space-x-2 text-lg">
+                    <input type="checkbox"  bind:group={selectedNames} class="w-6 h-6 mb-5 form-checkbox accent-primary-500" value=" I would like have more information about B2B solutions "/>
+                    <span class="mb-5">I would like have more information about B2B solutions</span>
+                </label>
+                <label class="flex items-center space-x-2 text-lg">
+                    <input type="checkbox"  bind:group={selectedNames} class="w-6 h-6 mb-5 form-checkbox accent-primary-500" value=" I would like to be contacted about setting up a B2B connection "/>
+                    <span class="mb-5">I would like to be contacted about setting up a B2B connection</span>
+                </label>
+                <label class="flex items-center space-x-2 text-lg">
+                    <input type="checkbox"  bind:group={selectedNames} class="w-6 h-6 mb-5 form-checkbox accent-primary-500" value=" I would like to know if there is an existing B2B connection for my organization "/>
+                    <span class="mb-5">I would like to know if there is an existing B2B connection for my organization</span>
+                </label>
+                <label class="flex items-center space-x-2 text-lg">
+                    <input type="checkbox"  bind:group={selectedNames} class="w-6 h-6 mb-5 form-checkbox accent-primary-500" value=" I have another question (fill in below Additional details)"/>
+                    <span class="mb-5">I have another question (fill in below Additional details)</span>
+                </label>
             </div>
                 <div class="w-full mt-3 max-w-full sm:max-w-lg md:max-w-3xl overflow-hidden">
                     <div class="flex flex-col md:flex-row md:space-x-4">
@@ -329,14 +334,23 @@
                             </div>
                             <div class="flex-1 mb-4 w-5/6" >
                             </div>                                    
-
-
                         </div>                                    
                         <div class="flex-1 mb-4 w-5/6 sm:w-full">
                             <textarea name="details" id="details" bind:value={details} class="w-full p-3 border border-gray-300  focus:outline-none focus:border-primary-500 focus:shadow-none focus:ring-0 placeholder-gray-400 mb-4 h-32" placeholder="Additional Details*"></textarea>
                             {#if showErrors && details.length === 0}
                                 <span class="text-red-400 text-xs">Additional Details is required</span>
                             {/if}
+                        </div>
+                        <div>
+                            <label class="flex items-center space-x-2">
+                                <input 
+                                    type="checkbox" 
+                                    name="user_feedback" 
+                                    bind:value={reason} 
+                                    class="w-6 h-6 mb-5 form-checkbox accent-primary-500"
+                                />
+                                <span class="mb-5 text-lg">I agree to the terms and conditions</span>
+                            </label>
                         </div>
                         <div class="flex-1 mb-4 sm:mt-0">
                             <button type="submit" on:click={handleSubmit} class="px-5 py-2 bg-primary-500 text-white rounded-lg transition duration-300 hover:bg-primary-300 sm:w-auto">Send Message</button>
