7d5a357863da41cd9742800851a61749894d1ca7
avinash <avinash@partskeys.com>
2024-11-18 17:21:22 +0530

    added some pages

---
+++ src/lib/components/ServiceUnavilable.svelte
@@ -0,0 +1,11 @@
+<div class="flex flex-col justify-center py-32 bg-gray-50">
+	<div class="text-8xl text-center font-roboto mb-5">500</div>
+	<div class="text-lg text-center font-medium font-sans mb-2">Service Unavailable</div>
+	<div class="text-lg text-center font-sans mb-7">
+		servers are experiencing high traffic,Please try again later.
+	</div>
+	<div class="flex justify-center">
+		<a class="text-sm bg-primary-500 text-white font-semibold py-2 px-9 rounded-full hover:bg-primary-600 font-sans"
+			href="/">Back to Home</a>
+	</div>
+</div>
---
+++ src/stories/main/Returns.stories.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	import Returns from '$lib/components/Returns.svelte';
+
+	export const meta = {
+		title: 'Returns',
+		component: Returns
+	};
+</script>
+
+<script>
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+	import '../../app.css';
+</script>
+
+<Template let:args>
+	<Returns></Returns>
+</Template>
+
+<Story name="Returnspage" />
