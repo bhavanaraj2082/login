a5448a59c55b3eefd6d33a1fbfd073b8d48bc180
santhosh <srihari@partskeys.com>
2024-12-03 14:27:12 +0530

    Added promise

--- src/routes/products/[subid]/[subsubid]/[product]/+page.server.js
+++ src/routes/products/[subid]/[subsubid]/[product]/+page.server.js
@@ -1,17 +1,22 @@
 import { DifferentProds } from "$lib/server/mongoLoads.js";
 import { RelatedProductData } from "$lib/server/mongoLoads.js";
-export async function load({ params}) {
- const productNumber= params.product;
+
+export async function load({ params }) {
+  const productNumber = params.product;
   try {
-    const productData = await DifferentProds(params.product);
-    const RelatedProduct = await RelatedProductData(params.product); 
+    const [productData, relatedProducts] = await Promise.all([
+      DifferentProds(params.product),
+      RelatedProductData(params.product)
+    ]);
+
     if (productData.type === "error") {
       return {
         error: productData.message,
       };
     }
-return {productData ,relatedProducts : RelatedProduct}
-;} catch (error) {  
+
+    return { productData, relatedProducts };
+  } catch (error) {
     console.error("Error loading product data:", error);
     return {
       error: "Failed to load product data.",
