ececca908a00834d69f17f18dcff5068d7bd9412
santhosh <srihari@partskeys.com>
2025-03-11 11:57:11 +0530

    new route

---
+++ src/routes/products/details/[product]/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	import SEO from '$lib/components/SEO.svelte';
+    import ProductDetails from "$lib/components/ProductInfoPopups/ProductDetails.svelte";
+    import RelatedProductss from "$lib/components/RelatedProductss.svelte";
+    import CompSimItems from '$lib/components/CompSimItems.svelte'
+    import { PUBLIC_WEBSITE_URL,PUBLIC_WEBSITE_NAME } from '$env/static/public';
+
+    export let data;
+    // console.log("Data  ==>(:", data); 
+
+    let productData = data?.productData?.records[0];
+
+    let categoryUrl = data?.relatedProducts[0]?.categoryInfo[0]?.urlName
+
+    let subCategoryUrl = data?.relatedProducts[0]?.subCategoryInfo[0]?.urlName
+    
+	let metadata = {
+		title: `${productData?.productName} | ${productData?.manufacturer?.name} | ${PUBLIC_WEBSITE_NAME} ` || 'Default Product Title',
+		description: `Order ${productData?.manufacturer?.name} manufacturers ${productData?.productName} partnumber online at ${PUBLIC_WEBSITE_NAME}.` || 'Default product description',
+		keywords: `${productData?.manufacturer?.name}, ${productData?.productName},${productData?.description}` || 'default, product, keywords',
+		urlPath: `${PUBLIC_WEBSITE_URL}/products/${categoryUrl}/${subCategoryUrl}/${productData?.productNumber}` || 'Default URl',
+		image: productData?.imageSrc,
+		// ? `${PUBLIC_COMPBUY_IMAGE_PATH}/part/${data.componentData.image}`
+	    // 	: '/partskeys.jpeg',
+		schema: {
+			"@context": "https://schema.org",
+			"@type": "Product",
+			"name": `${productData?.productName} | ${productData?.manufacturer?.name} | ${PUBLIC_WEBSITE_NAME} `|| 'Default Product Name',
+			"description":`Order ${productData?.manufacturer?.name} manufacturers ${productData?.productName} partnumber online at ${PUBLIC_WEBSITE_NAME}.` || 'Default product description',
+			"url": `${PUBLIC_WEBSITE_URL}/products/${categoryUrl}/${subCategoryUrl}/${productData?.productNumber}` || 'Default URl',
+			"brand": {
+    		"@type": "Brand",
+    		"name": `${PUBLIC_WEBSITE_NAME}`
+  		},
+		}
+	};
+ 
+</script>
+
+<SEO {metadata} />
+
+<ProductDetails data={data.productData} isFavorite={data.isFavorite} isauthedUser={data.authedUser}/>
+
+{#if data.relatedProducts.length !== 0}
+    <RelatedProductss relatedProducts={data.relatedProducts}/>  
+{/if} 
+
+{#if data.compareSimilarity.length !== 0}
+    <CompSimItems compareSimilarity={data.compareSimilarity}/>
+{/if} 
