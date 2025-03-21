7a1fc2e464ebaed4f5b6c1742cadff0593a3f42b
avinash <avinash@partskeys.com>
2025-03-21 11:28:56 +0530

    added uncategorized image

--- src/lib/components/Terms.svelte
+++ src/lib/components/Terms.svelte
@@ -1,40 +1,36 @@
 <script>import Icon from '@iconify/svelte';</script>
 
-<div class="lg:w-11/12 max-w-7xl mx-auto px-6 sm:px-8 md:px-10">
+<div class="lg:w-11/12 max-w-7xl mx-auto px-6 md:px-4">
     <h1  class="font-bold text-3xl mb-5 ">Terms</h1>
-<p class="text-base md:mb-7">For legal statements and documentation, please visit the links below for our terms & conditions, site use terms, privacy policy, product license.</p>
+<p class="md:text-base text-sm md:mb-7 mb-4 text-justify">For legal statements and documentation, please visit the links below for our terms & conditions, site use terms, privacy policy, product license.</p>
 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-2">
-  <div class="space-y-4">
+  <div class="space-y-4 shadow p-4 rounded-md bg-white hover:shadow-md transition-all duration-300">
     <a href="/terms/conditions" class="font-bold text-primary-400 flex items-center hover:underline">
-      <Icon icon="material-symbols:admin-panel-settings-outline" class="mr-2 text-3xl text-primary-500" />
+      <Icon icon="material-symbols:policy-outline-rounded" class="mr-2 md:text-4xl text-3xl text-primary-500" />
       Terms & Conditions
     </a>
-    <p class="text-sm">
+    <p class="md:text-sm text-xs text-justify">
       These terms and conditions of sale, any Sales Documents accompanying or referencing these Terms, and Supplemental Terms, if any, comprise the entire agreement between Chemikart, Inc. or an affiliate thereof and the purchaser with respect to the purchase and sale of products and services indicated on Sales Documents.
     </p>
   </div>
-  
-    
-    <div class="space-y-4">
+    <div class="space-y-4 shadow p-4 rounded-md bg-white hover:shadow-md transition-all duration-300">
       <a  href="/terms/site-and-terms"  class="font-bold text-primary-400 flex items-center hover:underline ">
-          <Icon icon="material-symbols:assignment-turned-in-outline-sharp" class="mr-2 text-3xl text-primary-500" />
+          <Icon icon="material-symbols:assignment-turned-in-outline-rounded" class="mr-2 md:text-4xl text-3xl text-primary-500" />
           Site Use Terms
         </a>
-      <p class="text-sm">This Site provides users with access to extensive product information and documentation as well as online purchasing capabilities. These Site Use Terms ("Terms") set forth the terms and conditions under which you may use and access this Site.</p>
+      <p class="md:text-sm text-xs text-justify">This Site provides users with access to extensive product information and documentation as well as online purchasing capabilities. These Site Use Terms ("Terms") set forth the terms and conditions under which you may use and access this Site.</p>
     </div>
-    
-    <div class="space-y-4 md:mb-10">
+    <div class="space-y-4 md:mb-10 shadow p-4 rounded-md bg-white hover:shadow-md transition-all duration-300">
       <a href="/terms/product-license"  class="font-bold text-primary-400 flex items-center hover:underline">
-        <Icon icon="material-symbols-light:productivity-outline" class="mr-2 mt-2 text-4xl  text-primary-500" />
+        <Icon icon="mdi:drivers-license-outline" class="mr-2 md:text-4xl text-3xl text-primary-500" />
         Product Licenses</a>
-      <p class="text-sm">Learn more about licensing requirements for some of our catalog and custom products, including our Mission RNA products.</p>
+      <p class="md:text-sm text-xs text-justify">Learn more about licensing requirements for some of our catalog and custom products, including our Mission RNA products.</p>
     </div>
-  
-    <div class="space-y-4 md:mb-10">
+    <div class="space-y-4 md:mb-10 shadow p-4 rounded-md bg-white hover:shadow-md transition-all duration-300">
       <a href="/terms/privacy-notice"  class="font-bold text-primary-400 flex items-center hover:underline">
-        <Icon icon="stash:payment-link-light" class="mr-2 text-4xl text-primary-500" />
+        <Icon icon="iconoir:privacy-policy" class="mr-2 md:text-4xl text-3xl text-primary-500" />
         Privacy Statement</a>
-      <p class="text-sm ">With this data protection declaration, we explain to you which personal data is processed for which purposes.</p>
+      <p class="md:text-sm text-xs text-justify">With this data protection declaration, we explain to you which personal data is processed for which purposes.</p>
     </div>
   </div>
 </div>
