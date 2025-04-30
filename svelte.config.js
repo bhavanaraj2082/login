5216972c9d1ebda560f46c0b960b0ade520d51c8
Soumyaranjan Pradhan <soumya@partskeys.com>
2025-04-30 13:20:22 +0530

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			out: 'out',
+			precompress: true,
+			envPrefix: 'CHEMIKART_',
+			assets: 'static'
+		}),
+		csrf: {
+			checkOrigin: false
+		}
 	},
-	preprocess: vitePreprocess()
+	preprocess: [vitePreprocess(), sequence([preprocessMeltUI()])]
 };
-
 export default config;
