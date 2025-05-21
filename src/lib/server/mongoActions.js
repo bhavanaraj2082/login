e3dd0a9e5167ade72ee927a5147790565ce49147
yusuf <yusuf@partskeys.com>
2025-05-21 13:20:35 +0530

    fixed bugs

--- src/lib/server/mongoActions.js
+++ src/lib/server/mongoActions.js
@@ -2276,7 +2276,7 @@ export const favaddToCart = async (cartData, userId, userEmail) => {
 			);
 
 			if (itemIndex > -1) {
-				if (existingCart.cartItems[itemIndex].isCart === false && existingCart.cartItems[itemIndex].isQuote) {
+				if (existingCart.cartItems[itemIndex].isCart === false && existingCart.cartItems[itemIndex].isQuote === false) {
 					existingCart.cartItems[itemIndex].quantity += parseInt(cartData.quantity) || 1;
 					await existingCart.save();
 					return {
@@ -3124,7 +3124,7 @@ export const getGuestCart = async (body) => {
 		// Fetch all product details in a single query
 		const productMapPromise = Product.find(
 			{ _id: { $in: productIds } },
-			{ imageSrc: 1, productName: 1, productNumber: 1 }
+			{ image: 1, productName: 1, productNumber: 1 }
 		).then(products =>
 			products.reduce((acc, p) => {
 				acc[p._id] = p;
